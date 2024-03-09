9a1b8a4172d5a81f499d0937ad29de1c44d42727
Gustavo Marques <142382220+Gustavo-Marques-Santiago-Sanches@users.noreply.github.com>
2024-03-08 22:16:46 -0300

    Tabela Finalizada

--- assets/js/calcula_total.js
+++ assets/js/calcula_total.js
@@ -10,16 +10,25 @@ for (var count=0; count < clientes.length; count++){
     var unitario = clientes[count].querySelector(".valor").textContent;
 
     //Verifica se a quantidade é valida
-    if (quant < 1 || isNaN(quant)){
+    if (!validaQuant(quant)){
         //Quantidade NOK, avisa usuário
         clientes[count].querySelector(".quant").textContent = "QUANTIDADE INVALIDA!";
-        clientes[count].style.color="white";
-        clientes[count].style.backgroundColor="red";
-    } else{
+        clientes[count].querySelector(".quant").classList.add("quant-invalida");
+        clientes[count].querySelector(".valor").textContent = format(parseFloat(unitario));
+    }
+    else if(!validaUni(unitario)){
+        //Valor Unitario NOK, avisa usuário
+        clientes[count].querySelector(".valor").textContent = "VALOR INVALIDO!";
+        clientes[count].classList.add("unit-invalida");
+    } 
+    else{
         //Quantidade OK, prosseguir
         //Captura o valor da encomenda
         clientes[count].querySelector(".total").textContent = calculaTotal(quant, unitario);
+        clientes[count].querySelector(".valor").textContent = format(parseFloat(unitario));
     }
+    
+    
 }
 
 //Função para o calculo do valor total
@@ -28,5 +37,27 @@ function calculaTotal(quantEncomenda, unitarioProduto){
 
     total = quantEncomenda * unitarioProduto;
 
-    return total;
+    return format(total);
+}
+
+function validaQuant(quant){
+    if(quant > 0 && !isNaN(quant)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaUni(unit){
+    if(unit > 0 && !isNaN(unit)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function format(val){
+    var valor = val.toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
+
+    return valor;
 }
