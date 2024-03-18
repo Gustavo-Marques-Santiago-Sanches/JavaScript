a066f607de43d675ea64968a30fc971ea843bd9c
Gustavo Marques <142382220+Gustavo-Marques-Santiago-Sanches@users.noreply.github.com>
2024-03-18 16:37:27 -0300

    3.0
    Calculo do valor total funcionando

--- assets/js/add_tabela.js
+++ assets/js/add_tabela.js
@@ -1,6 +1,5 @@
 var botaoAdd = document.querySelector("#bot");
 
-
 botaoAdd.addEventListener("click", function(event){
     event.preventDefault();
     
@@ -8,26 +7,27 @@ botaoAdd.addEventListener("click", function(event){
     var prod = document.getElementById("produto").value;
     var quant = document.getElementById("quantidade").value;
     var val = document.getElementById("val_unit").value;
-    
     var tabela = document.querySelector("#tabela");
+
     var linha = document.createElement("tr");
     var colunaNome = document.createElement("td");
     var colunaProd = document.createElement("td");
     var colunaQuant = document.createElement("td");
     var colunaVal = document.createElement("td");
+    var colunaTotal = document.createElement("td");
 
     colunaNome.textContent = nome;
     colunaProd.textContent = prod;
     colunaQuant.textContent = quant;
     colunaVal.textContent = format(parseFloat(val));
+    colunaTotal.textContent = calculaTotal(quant, val);
 
     linha.appendChild(colunaNome);
     linha.appendChild(colunaProd);
     linha.appendChild(colunaQuant);
     linha.appendChild(colunaVal);
-    
-    return tabela.appendChild(linha);
+    linha.appendChild(colunaTotal);
 
-    //formata_tabela(nome, prod, quant, val);
+    return tabela.appendChild(linha);
 });
 
